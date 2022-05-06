a774fc753917a112e13f1a1a56358f0dd0417475
Marco Carminati <marco.carminati@traxretail.com>
2022-05-06 12:38:29 +0200

    created frontend + connection flask

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { getHttp } from './http/fetchApi';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    getHttp('http://127.0.0.1:5000/toldyou', 'json').then((response) => { console.log(response); })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- server/app.py
+++ server/app.py
@@ -1,17 +1,24 @@
 from flask import Flask, jsonify
-from toldyou.models import ToldYou
-## connection via mongodb.
-from mongoengine import connect
-connect(host="mongodb://127.0.0.1:27017/toldyou")
+from flask_cors import CORS
+
+# from toldyou.models import ToldYou
+# ## connection via mongodb.
+# from mongoengine import connect
+# connect(host="mongodb://127.0.0.1:27017/toldyou")
 
 ## Init app
 
 app = Flask(__name__)
+CORS(app)
 
+@app.route('/')
+def get_status():
+  return {"status": "ok"}
 
 @app.route('/toldyou')
 def get_toldyou():
-  return ToldYou.objects.to_json()
+  return {"results":"[{\"_id\": {\"$oid\": \"6274f219d391ef6e993799d8\"}, \"name\": \"test\", \"users\": []}]"}
+  # return {"results": ToldYou.objects.to_json()} 
 
 
 # Start the app
