250fd3984094cd58db65b64cfe1338fea9f41f55
Marco Carminati <marco.carminati@traxretail.com>
2022-05-08 19:40:23 +0200

    integration naiveui

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <n-loading-bar-provider>
+  <n-message-provider>
+    <n-notification-provider>
+      <n-dialog-provider>
   <HelloWorld msg="Welcome to Your Vue.js App" />
+      </n-dialog-provider>
+    </n-notification-provider>
+  </n-message-provider>
+</n-loading-bar-provider>
+    <n-button>naive-ui</n-button>
+ 
 </template>
 
 <script>
+
+// General Font
+// Monospace Font
+import 'vfonts/FiraCode.css'
 import HelloWorld from './components/HelloWorld.vue'
 import { getHttp } from './http/fetchApi';
+
 
 export default {
   name: 'App',
@@ -13,7 +28,8 @@ export default {
     HelloWorld
   },
   setup() {
-    getHttp(`${process.env.VUE_APP_REST_URL}/toldyou`, 'json').then((response) => { console.log(response); })
+    console.log(`${process.env.VUE_APP_REST_URL}/toldyou`)
+    getHttp(`${process.env.VUE_APP_REST_URL}/toldyou`, 'json').then((response) => { console.log("response", response); })
   }
 }
 </script>
